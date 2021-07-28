bc569d5c5b8e460630219e026d7ac625c5555572
Walter Derezinski <walter@daringway.com>
2021-07-28 15:29:42 -0400

    updates

--- provision.sh
+++ provision.sh
@@ -1,16 +1,9 @@
 #!/bin/bash -x
 
-sudo mv /tmp/rc.local /etc/rc.local
-chmod +x /etc/rc.local
-
 mkdir /var/www
-cd /var/www
 
-git clone --single-branch https://github.com/daringway/aws-ec2-cli-tools.git
-bash ./aws-ec2-cli-tools/setup.sh
-
-https://github.com/daringway/ghost-manager.git /var/www/manager
-bash /var/www/manager/setup.sh
+git clone --single-branch https://github.com/daringway/ghost-serverless. /var/www/ghost-serverless
+bash /var/www/ghost-serverless/setup.sh
 
 mv /tmp/templates /var/www
 
@@ -22,24 +15,15 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 sudo apt-get update
 sudo apt-get upgrade
 
-curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 sudo apt-get install -y ec2-instance-connect nodejs zip nginx yarn jq fish
 sudo snap install --classic aws-cli
-sudo snap install --classic certbot
-
-sudo ln -s /snap/bin/certbot /usr/bin/certbot
 
 # install ghost
-sudo npm install ghost-cli@latest eslint ghost-static-site-generator -g
+sudo npm install ghost-cli@latest -g
 
 # Setup firewall
 sudo ufw allow 'Nginx Full'
-
-# Install ghost publisher
-sudo npm install pm2@latest -g
-pm2 startup systemd
-
-
 
 ######Download and install Ghost######
 sudo mkdir -p /var/www/ghost
@@ -49,7 +33,6 @@ cd /var/www/ghost
 ghost install local
 ghost setup linux-user systemd
 sudo rm -r /var/www/ghost/config.development.json /var/www/ghost/content /var/www/ghost/current
-
 
 # TODO run ghost install
 # TODO configure ghost for mailgun https://ghost.org/docs/concepts/config/#setup-an-email-sending-account
