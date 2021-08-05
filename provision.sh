cd664714568cadeadf37a9eb81f6cebff455d260
Scott Griggs <sgriggs@gmail.com>
2021-08-05 14:45:03 -0400

    Moved some content to rc.local

--- provision.sh
+++ provision.sh
@@ -4,7 +4,6 @@
 # Leverages https://pm2.keymetrics.io/docs/usage/pm2-doc-single-page/
 
 INSTALL_DIR=/var/www/ghost-serverless
-RELEASE_VERSION=main
 NODE_VERSION=14
 
 if [[ $(id -u) != "0" ]]
@@ -13,8 +12,9 @@ then
   exit 2
 fi
 
-START_TS=$(date +%s)
-echo "ghost-serverless ts 0: starting"
+# Copy rc.local
+cp /tmp/rc.local /etc/rc.local
+chmod +x /etc/rc.local
 
 # Add Repos
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
@@ -37,59 +37,3 @@ chsh -s /usr/bin/fish ubuntu
 
 ###### Download ghost serverless ######
 git clone --single-branch https://github.com/daringway/ghost-serverless $INSTALL_DIR
-
-echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): core packages installed"
-
-#while ! aws sts get-caller-identity
-#do
-#  echo "Missing IAM Role or not attached , sleeping 15"
-#  sleep 15
-#done
-
-#echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): AWS creds verified"
-
-# Setup the .env
-#$INSTALL_DIR/bin/update-env
-#source $INSTALL_DIR/.env
-#
-#hostname $( echo $CMS_HOSTNAME | tr . - )
-
-# Want to setup the DNS record early so DNS has time to update
-#IP=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
-#TTL=60 # It currently takes about 2 minutes for the rest of the setup
-#aws route53 change-resource-record-sets --hosted-zone-id $ZONE_ID --change-batch '{"Changes":[{"Action":"UPSERT","ResourceRecordSet":{"Name":"'$CMS_HOSTNAME'","Type":"A","TTL":'$TTL',"ResourceRecords":[{"Value":"'$IP'"}]}}]}'
-#
-#echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): DNS updated"
-
-#echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): all packages installed"
-
-# Setup firewall
-#ufw allow 'Nginx Full'
-
-#for DIR in $(ls -d $INSTALL_DIR/services/*)
-#do
-#  (cd $DIR; npm install)
-#done
-
-#echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): ghost-serverless npm packages installed"
-#
-#mkdir -p /var/www/ghost $INSTALL_DIR $WEB_DIR $BACKUP_DIR
-#chown -R ubuntu:ubuntu /var/www /home/ubuntu $INSTALL_DIR
-#chmod 775 /var/www/ghost
-
-# TODO update nginx upload limit
-
-# curl https://get.acme.sh | sh -s email=certs@daringway.com --home /etc/letsencrypt/
-# Wrap this in a webserver so you can see the status from the browser (Or at least give steps and not full logs)
-#su ubuntu -c $INSTALL_DIR/bin/site-restore
-
-#echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): ghost site restored"
-
-# Setup temp nginx to status service
-# 1st restore site (not version)
-
-# Setup ghost-serverless services
-#su ubuntu -c "cd $INSTALL_DIR; pm2 start ecosystem.config.js"
-
-#echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): ghost-serverless started"
-#echo "ghost-serverless ts $(( $(date +%s) - $START_TS )): done"
